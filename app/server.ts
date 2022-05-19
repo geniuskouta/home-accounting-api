a7c7c3a4689247f4206b4555d23e2d23eb250f25
Kouta Nakano <knakano95@gmail.com>
2022-05-19 23:06:42 +0900

    set up router

--- app/server.ts
+++ app/server.ts
@@ -1,6 +1,10 @@
 import 'dotenv/config';
 import express from 'express';
+import appRouter from './src/routes/app.router';
+
 const server = express();
+
+server.use('/', appRouter);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
