07c0362d109325b926ba8c07372bcc2f4cd1424f
Kouta Nakano <knakano95@gmail.com>
2022-05-19 22:39:04 +0900

    express postgres typescript setup

---
+++ app/server.ts
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+import express from 'express';
+const server = express();
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Listening to ${PORT}`);
+})
